c0c9d918cc92b85dfb397466027267d68059c375
Atakan Gemici <atakan.gemici@ofisim.com>
2021-04-01 12:06:22 +0300

    fixed.

---
+++ Sample.Model/Models/BaseEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace urunKovaniApi.Models
+{
+    public class BaseEntity
+    {
+        [Column("id"), Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column("owner")]
+        public int? Owner { get; set; }
+
+        [Column("created_by")]
+        public int? CreatedById { get; set; }
+
+        [Column("updated_by")]
+        public int? UpdatedById { get; set; }
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("updated_at")]
+        public DateTime? UpdatedAt { get; set; }
+
+        [Column("deleted")]
+        public bool Deleted { get; set; }
+
+    }
+}
